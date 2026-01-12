fe820fabbf88d1e5b11218963193be4d8c6be02d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:23:08 +0000

    5b0e705 docs: improve separation between enter and leave example

--- index.js
+++ index.js
@@ -363,12 +363,12 @@ function requireOverride(moduleName) {
   return originalRequire(moduleName);
 }
 requireOverride.resolve = originalRequire.resolve;
-/**
+/*!
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 require = requireOverride;
 //# sourceMappingURL=banner.js.map
@@ -248159,12 +248159,12 @@ function main() {
   session.listen();
 }
 main();
-/**
+/*!
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 /*! Bundled license information:
 
